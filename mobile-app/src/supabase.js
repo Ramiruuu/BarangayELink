49ebf38b60001027402e8f0dccbeb08b042854e5
Ramiruuu <oclaritremar25@gmail.com>
2026-06-01 11:08:40 -0400

    deleted mobile

--- mobile-app/src/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://nigslnqsyfcacjeknwyh.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5pZ3NsbnFzeWZjYWNqZWtud3loIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAyMzE2MzIsImV4cCI6MjA5NTgwNzYzMn0.D9iceNFDyUrYhNARzFOID_a0lbv-A91tMVg0yod-_JQ';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
